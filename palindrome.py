98cc0934f2043803c49dc6b7637cd8f8a3fec20c
Sonasunils <sona2002sunil@gmail.com>
2024-05-21 14:01:24 +0530

    for loop added

--- palindrome.py
+++ palindrome.py
@@ -1,20 +1,21 @@
 def palindrome(n):
     
-    # n=n.lower()             #converted into lowercase
-    # k=n[::-1]               #reverse the word into variable k
-    # if n==k:                         #check the if condition where user enterd word and reversed word are same
+    # n=n.lower()                           #converted into lowercase
+    # k=n[::-1]                             #reverse the word into variable k
+    # if n==k:                              #check the if condition where user enterd word and reversed word are same
     #     print(n,"is palindrome")
     # else:
     #     print(n,"not palindrome")
     
-    
+    # 2nd method using for loop
+
     n=n.lower()
-    l=len(n)
+    l=len(n)                                #get length of the string
     s=""
     for i in range(l-1,-1,-1):
        
        s+=n[i]
-    if s==n:                         #check the if condition where user enterd word and reversed word are same
+    if s==n:                               #check the if condition where user enterd word and reversed word are same
         print(n,"is palindrome")
     else:
         print(n,"not palindrome")
